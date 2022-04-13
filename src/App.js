bbb1fd17f9f503405732be094316462106ea75e1
Bryanmiessler <bryanmiessler@gmail.com>
2022-04-13 11:52:52 -0500

    creating header component and putting on app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           ¡Hola Mundo!
@@ -13,7 +15,7 @@ function App() {
     </div>
   );
 }
-
+console.log("Hola mundo");
 
 
 export default App;
