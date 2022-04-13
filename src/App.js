24f60292065b23648041971ec2d26bab3557ad07
Bryanmiessler <bryanmiessler@gmail.com>
2022-04-13 09:55:09 -0500

    First Changes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¡Hola Mundo!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
