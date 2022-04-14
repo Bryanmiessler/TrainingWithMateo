8bf1a19ff16be208e36c0217e191bad28d233e83
Bryanmiessler <bryanmiessler@gmail.com>
2022-04-14 12:29:56 -0500

    creating square with text in the middle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ¡Hola Mundo!
-        </p>
       </header>
+      <body>
+        <Card />
+      </body>
       <footer>
         <Footer />
       </footer>
     </div>
   );
 }
-console.log("Hola mundo");
-
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Card.css';
+
+function Card() {
+    return(
+        <div className="SquareFather">
+            <div className="SquareSon">
+                <h2 className="TextSquare">Esto es un cuadrado que no consume drogas</h2>
+            </div>
+        </div>
+    );   
+}
+
+export default Card;
