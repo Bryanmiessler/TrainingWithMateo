db6f6c6b1872b83eec9d5cd290b40ba73184ee71
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-04-25 20:21:32 +0200

    New eercise

---
+++ src/training/cars.js
@@ -0,0 +1,22 @@
+const cars = [
+  { id: 1, name: "Ford", color: "red", price: 20000 },
+  { id: 2, name: "Chevrolet", color: "blue", price: 15000 },
+  { id: 3, name: "Nissan", color: "green", price: 10000 },
+  { id: 4, name: "Honda", color: "red", price: 5000 },
+  { id: 5, name: "Toyota", color: "black", price: 25000 },
+  { id: 6, name: "Mazda", color: "black", price: 30000 },
+  { id: 7, name: "Mercedes", color: "silver", price: 40000 },
+  { id: 8, name: "Audi", color: "orange", price: 35000 },
+  { id: 9, name: "BMW", color: "purple", price: 45000 },
+  { id: 10, name: "Lexus", color: "pink", price: 55000 },
+];
+
+// Returns cars with price greater than 30000 (filter only)
+
+// Returns cars with price lower than 30000 (filter only)
+
+// Returns cars with red color (filter only)
+
+// Returns cars with black color (filter only)
+
+// Return cars without id, example { name: 'Lexus', color: "pink", price: 55000 } (map only)
