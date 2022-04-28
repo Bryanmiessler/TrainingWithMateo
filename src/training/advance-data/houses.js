ae13004a99fd3f55041ee98547c5a64275f45d0d
Bryanmiessler <bryanmiessler@gmail.com>
2022-04-27 20:19:21 -0500

    houses exercise

--- src/training/advance-data/houses.js
+++ src/training/advance-data/houses.js
@@ -1,3 +1,5 @@
+
+
 const houses = [
     { id: 1, city: 'Medellin', state: 'Antioquia', size: 100, price: 2000000, sellerId: 1 },
     { id: 2, city: 'Bogota', state: 'Cundinamarca', size: 200, price: 1500000, sellerId: 2 },
@@ -15,17 +17,82 @@ const houses = [
 ]
 
 // Filter houses with price greater than 500000
+const housesPriceGreaterThan500000 = houses.filter((house) => house.price > 500000);
+
+// console.log({housesPriceGreaterThan500000});
 
 // Filter houses with price lower than 500000
 
+const housesPriceLowerThan500000 = houses.filter((house) => house.price < 500000);
+
+// console.log({housesPriceLowerThan500000});
+
+
 // Map houses without id, example { city: 'Floridablanca', state: 'Bolivar', size: 1400, price: 4000000, sellerId: 4 }
+
+const withoutId = houses.map((house) => {
+    return {
+        city: house.city,
+        state: house.state,
+        size: house.size,
+        price: house.price,
+        sellerId: house.sellerId
+    }
+})
+
+// console.log({withoutId});
 
 // Map houses without id and price, example { city: 'Floridablanca', state: 'Bolivar', size: 1400, sellerId: 4 }
 
+const withoutIdAndPrice = houses.map((house) => {
+    return {
+        city: house.city,
+        state: house.state,
+        size: house.size,
+        sellerId: house.sellerId
+    }
+})
+
+// console.log({withoutIdAndPrice});
+
 // Map houses without id, price and city, example { state: 'Bolivar', size: 1400, sellerId: 4 }
+
+const withoutIdAndPriceAndCity = houses.map((house) => {
+    return {
+        state: house.state,
+        size: house.size,
+        sellerId: house.sellerId
+    }
+})
+
+// console.log({withoutIdAndPriceAndCity});
 
 // Filter houses with seller id 2
 
+const housesSellerId2 = houses.filter((house) => house.sellerId === 2);
+
+// console.log({housesSellerId2});
+
 // Group houses by city
 
+const groupByCity = houses.reduce((group, house) => {
+    const { city } = house;
+    group[city] = group[city] ?? [];
+    group[city].push(house);
+    return group;
+  }, {});
+  console.log(groupByCity);
+
+
 // Group houses by state
+
+const groupByState = houses.reduce((group, house) => {
+    const { state } = house;
+    group[state] = group[state] ?? [];
+    group[state].push(house);
+    return group;
+  }, {});
+
+  console.log(groupByState);
+
+
